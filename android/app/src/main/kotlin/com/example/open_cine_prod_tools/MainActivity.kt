8fabe71b399e01943c234d45c747d1ef8ecac332
brolandeau <losmuchachosdesarrolladores@gmail.com>
2024-12-22 19:48:08 +0100

    Create flutter app
    refs #3

---
+++ android/app/src/main/kotlin/com/example/open_cine_prod_tools/MainActivity.kt
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2024 Benoit Rolandeau <borlnov.obsessio@gmail.com>
+
+// SPDX-License-Identifier: MIT
+
+package com.example.open_cine_prod_tools
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ linux/runner/main.cc
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2024 Benoit Rolandeau <borlnov.obsessio@gmail.com>
+//
+// SPDX-License-Identifier: MIT
+
+#include "my_application.h"
+
+int main(int argc, char** argv) {
+  g_autoptr(MyApplication) app = my_application_new();
+  return g_application_run(G_APPLICATION(app), argc, argv);
+}
